5784abd7eeaa01d016773e0962efc43e6983de19
Kyle Hoac <hoackyle@gmail.com>
2021-04-23 18:00:28 -0700

    added cache to api calls

--- components/movie.js
+++ components/movie.js
@@ -8,19 +8,46 @@ class Movies {
         this.image = movie.poster_path;
     }
 }
-
+const inMemoryDB = {};
 async function movieHandler(request, response) {
     const cityName = request.query.cityName
-    const key = process.env.MOVIE_API_KEY
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${cityName}`
-
-    const movieResponse = await superagent.get(url);
-
-    const movieObject = JSON.parse(movieResponse.text);
-
-    const movieTitle = movieObject.results.map(movie => new Movies(movie));
+    try {
+        const movieAlreadyFound = inMemoryDB[cityName] !== undefined;
+        if (movieAlreadyFound) {
+            if (inMemoryDB[cityName].timestamp + 5000 < Date.now()) {
+                console.log('old movie data')
+                const key = process.env.MOVIE_API_KEY
+                const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${cityName}`
     
-    response.send(movieTitle);
-}
+                const movieResponse = await superagent.get(url);
+    
+                const movieObject = JSON.parse(movieResponse.text);
+    
+                const movieTitle = movieObject.results.map(movie => new Movies(movie));
+    
+                inMemoryDB[cityName] = { movies: movieTitle, timestamp: Date.now() }
+                response.status(200).send(inMemoryDB[cityName].movies);
+            } else {
+                console.log('good data')
+                const movies = inMemoryDB[cityName].movies;
+                response.status(200).send(movies);
+            }
+        } else {
+            console.log('no data')
+            const key = process.env.MOVIE_API_KEY
+            const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${cityName}`
 
+            const movieResponse = await superagent.get(url);
+
+            const movieObject = JSON.parse(movieResponse.text);
+
+            const movieTitle = movieObject.results.map(movie => new Movies(movie));
+
+            inMemoryDB[cityName] = { movies: movieTitle, timestamp: Date.now() }
+            response.status(200).send(inMemoryDB[cityName].movies);
+        }
+    } catch (error) {
+        response.send('error: Something went wrong!')
+    }
+}
 module.exports = movieHandler;
